b1f5e3f643c94411cee1c661f47fb2e70199f69e
Mark Delgado <markdelgado.md@hotmail.com>
2022-07-20 14:05:24 -0400

    added dynamic functionality

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import './App.css';
 import Fighter from './components/Fighter';
 
 function App() {
-const fighters = ['Roy','Ganondorf', 'Mario', 'Luigi', 'Marth', 'Link',
- 'Inkling', 'Samus', 'Fox', 'Sheik', 'Zelda', 'Ken']
 
+  const fighter = [
+    {name: 'Roy', color: 'blue'},
+    { name: 'Lucina', color: 'brown' },
+    { name: 'Mario', color: 'lightskyblue' },
+    { name: 'Luigi', color: 'pink' },
+    { name: 'Marth', color: 'dodgerblue' },
+    { name: 'Link', color: 'gray' },
+    { name: 'Inkling', color: 'orange' },
+    { name: 'Samus', color: 'gold' },
+    { name: 'Fox', color: 'mediumseagreen' },
+    { name: 'Sheik', color: 'coral' },
+    { name: 'Zelda', color: 'midnightblue' },
+    { name: 'Ken', color: 'firebrick' },
+  ]
   return (
     <div className="App">
       <h1>Fighters</h1>
       <div className='fighters-grid'>
         {
-          fighters.map((Element, index) =>{
+          fighter.map((Element, index) =>{
             return (
               <Fighter fighter= {Element}/>
             )
